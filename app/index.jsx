36d00be3c67f0ac849b00bb8d2da89a1a7e39c1b
Saleh <39927806+habtor@users.noreply.github.com>
2024-08-26 20:09:13 +0200

    added landing page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Text, View, Image } from "react-native";
+import { Link, Redirect, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-blue-600">
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ScrollView
+          contentContainerStyle={{
+            height: "100%",
+          }}
+        >
+          <View className="items-center justify-center w-full min-h-[85vh] px-4">
+            <Image
+              source={images.logo}
+              className="w-[130px] h-[84px]"
+              resizeMode="contain"
+            />
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[300px]"
+              resizeMode="contain"
+            />
+
+            <View className="relative mt-5">
+              <Text className="text-center text-white text-3xl font-bold">
+                Deiscover Endless Possibilities with{" "}
+                <Text className="text-secondary-200">Aora</Text>
+              </Text>
+              <Image
+                source={images.path}
+                className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                resizeMode="contain"
+              />
+            </View>
+            <Text className="text-center text-gray-100 mt-7 font-pregular text-sm">
+              Where creativity meets innovation: embark on a journey of
+              limitless exploration with Aora
+            </Text>
+            <CustomButton
+              title="Countinue with Email"
+              handlePress={() => router.push("/sign-in")}
+              containerStyles="w-full mt-7"
+            />
+          </View>
+        </ScrollView>
+        <StatusBar backgroundColor="#161622" style="light" />
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
