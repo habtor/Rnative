cfdab19bb47503116318b28d340afa1813c959dc
Saleh <39927806+habtor@users.noreply.github.com>
2024-09-06 22:46:52 +0200

    added search bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -38,7 +38,7 @@ const _layout = () => {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
       </Stack>
     </GlobalProvide>
   );
--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,12 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import { searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCart from "../../components/VideoCart";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
-export default Search
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCart video={item} />}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4">
+            <Text className="font-pmedium text-xs text-gray-100">
+              Search results
+            </Text>
+            <Text className="text-white text-2xl font-psemibold">{query}</Text>
+            <View className="mt-6 mb-8">
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No posts found"
+            subtitle="Np videos found for the search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Search;
